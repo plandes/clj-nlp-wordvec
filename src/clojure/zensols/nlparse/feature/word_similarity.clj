9dc15e3584e08cacf27147fd5245a395896c3042
Paul Landes <landes@mailc.net>
2018-02-21 21:32:48 -0600

    doc

--- src/clojure/zensols/nlparse/feature/word_similarity.clj
+++ src/clojure/zensols/nlparse/feature/word_similarity.clj
@@ -29,13 +29,19 @@
   (-> (glove-model)
       (.similarity a b)))
 
-(defn- similarity-tokens [dist-word text-tokens]
+(defn- similarity-tokens
+  "Return the cosine similarity across string tokens and a *distribution
+  word* or 0 if either is out of vocabulary."
+  [dist-word text-tokens]
   (->> text-tokens
        (map (fn [tok]
               (let [sim (similarity tok dist-word)]
                 (if (Double/isNaN sim) 0 sim))))))
 
-(defn- similarity-label [label dist text-tokens tok-count]
+(defn- similarity-label
+  "Return the similarity from a *distribution word* and all text tokens as a
+  weighted average with a partition function over the token length."
+  [dist text-tokens tok-count]
   (->> dist
        (map (fn [[dist-word weight]]
               (->> (similarity-tokens dist-word text-tokens)
@@ -43,21 +49,30 @@
                    (* weight))))
        (apply +)))
 
-(defn similarity-distribution [tokens word-count-stats]
+(defn similarity-distribution
+  "Return the similarity from all *distribution words* in **word-count-stats**
+  and all tokens as a weighted average with a partition function over the token
+  length."
+  [tokens word-count-stats]
   (let [text-tokens (map #(-> % :text s/lower-case) tokens)
         tok-count (count text-tokens)
         word-count-stats (:word-count-dist word-count-stats)]
     (->> word-count-stats
          (map (fn [[label dist]]
-                (let [p-hat (similarity-label label dist text-tokens tok-count)]
+                (let [p-hat (similarity-label dist text-tokens tok-count)]
                   [label (/ p-hat tok-count)])))
          (sort #(compare (second %2) (second %1))))))
 
-(defn similarity-features [tokens word-count-stats]
+(defn similarity-features
+  "Create similarity features using the MLE take from
+  the [[similarity-distribution]]."
+  [tokens word-count-stats]
   (->> (similarity-distribution tokens word-count-stats)
        first
        (zipmap [:similarity-top-label :similarity-score])))
 
-(defn similarity-feature-metas [labels]
+(defn similarity-feature-metas
+  "Return the metadata for [[similarity-features]]."
+  [labels]
   [[:similarity-top-label labels]
    [:similarity-score 'numeric]])
